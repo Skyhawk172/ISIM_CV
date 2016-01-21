431489b129f957a4e9dd9b99ad40d43c4f0266fd
Charles-Philippe Lajoie <lajoiecp@gmail.com>
2016-01-21 16:23:06 -0500

    First commit of existing script to process FITS files

---
+++ fix_isim.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+import pyfits
+import argparse
+import glob, os, sys
+
+
+def update_one_file(filename):
+    """ Open a FITS file and rewrite each header in it.
+    This does not actually apply any semantic changes to the file, 
+    but does reformat the header to be properly left-aligned for
+    cases where the Mathematica FITS writer has made them right-aligned. 
+
+    -MP 2011-08-23
+    """
+    print "Updating file "+filename+"...",
+    fits = pyfits.open(filename, mode='update')
+
+
+    # FIRST: UPDATE A FEW KEYWORDS FOR THE WAS:
+    fits[0].header.update("APERNAME", "APERNAME") 
+    if fits[1].header["EXTNAME"]!="SCI": fits[1].header.update("EXTNAME", "SCI")
+
+    # FIX SUBARRAY keyword if not correct (CV-3 data looks alright; CV-2 was hardcoded to False):
+    if fits[0].header["INSTRUME"]=="MIRI": full_dim=1032
+    else: full_dim=2048
+
+    if fits[1].data.shape[0]==full_dim and fits[1].data.shape[1]==full_dim: 
+        if fits[0].header["SUBARRAY"] != False: 
+            fits[0].header.update("SUBARRAY", False)
+            print "  ---> fixed subarray keyword"
+    else: 
+         if fits[0].header["SUBARRAY"] != True: 
+            fits[0].header.update("SUBARRAY", True)
+            print "  ---> fixed subarray keyword"
+
+    
+
+
+
+    # SECOND: ADD A FEW REQUIRED KEYWORDS FOR THE WAS:
+    if ("PUPIL" in fits[0].header) and (fits[0].header["PUPIL"]=="Weak Lens 1"): fits[0].header.update("PUPIL","WLP8") 
+    fits[0].header.set("SUBSTRT1",int(fits[0].header["COLCORNR"]))
+    fits[0].header.set("SUBSTRT2",int(fits[0].header["ROWCORNR"]))
+    fits[0].header.set("SUBSIZE1",fits[1].data.shape[0]) 
+    fits[0].header.set("SUBSIZE2",fits[1].data.shape[1])
+
+
+    # Add FGS-specific keywords:
+    if fits[0].header["DETECTOR"]=="GUIDER1": fits[0].header.set("FOCUSPOS", fits[0].header["G1FOCPOS"])
+    if fits[0].header["DETECTOR"]=="GUIDER2": fits[0].header.set("FOCUSPOS", fits[0].header["G2FOCPOS"])     
+
+
+    # Add NIRISS-specific keywords:
+    if fits[0].header["DETECTOR"]=="NIRISS":
+        fits[0].header.set("FOCUSPOS", fits[0].header["FMCCUPOS"])
+   
+
+    # Add NIRSpec-specific keywords:
+    if (fits[0].header["DETECTOR"]=="NRS1") or (fits[0].header["DETECTOR"]=="NRS2"):
+        fits[0].header.set("SLIT","")
+        fits[0].header.set("FCSRLPOS", fits[0].header["RMA_STEP"])
+        fits[0].header.set("RMA_POS", fits[0].header["RMA_MIC"])
+        fits[0].header.set("FOCUSPOS", fits[0].header["CIMGFSOF"])
+
+
+    # SAVE CHANGES TO FILE:
+    fits.flush() 
+    fits.close()
+    print "OK"
+
+
+def update_all(dir, string):
+    """ Update all files in a directory"""
+    os.chdir(dir)
+    print os.getcwd()+"/"+dir
+    for filename in glob.glob('*'+string+'*.fits'):
+        update_one_file(filename)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Fix FITS file headers for the WAS")
+
+    parser.add_argument("dir", metavar="dir", nargs=1, help="directory to process FITS files")
+    parser.add_argument("-s",  default='', metavar="string"  , help="string to match anywhere in filenames")
+
+    args = parser.parse_args()
+    dir=args.dir[0]
+    string=args.s
+
+    update_all(dir, string)
+
